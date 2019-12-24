0a3f1b30ec0ce91b2e59219b2fa16ef18d3e4d66
iamtahirkhan6 <48375147+iamtahirkhan6@users.noreply.github.com>
2019-12-25 02:48:57 +0530

    Fix #3.3

--- Code/admin/assign_teacher.php
+++ Code/admin/assign_teacher.php
@@ -1,88 +1,84 @@
 <?php
-// Start session
-session_start();
-
 // Import main class
-require_once "../classes/admin.class.php";
-require_once "../classes/structure.class.php";
+require "../classes/admin.class.php";
+require "../classes/structure.class.php";
 
-// Initialize HTML structure
-$struct = new Structure();
+// Start session
+Session::init();
 
 // Check if logged in otherwise redirect to login page
-$struct->checkLogin();
+Structure::checkLogin();
 
 // Load Header
-$struct->header("Assign Teacher To Student - Admin");
+Structure::header("Assign Teacher To Student - Admin");
 
-if (isset($_POST["teacher_id"])) {
-    if (count($_POST["students"]) > 0) {
-        foreach ($_POST["students"] as $student_id) {
+if (Structure::if_all_inputs_exists(array("teacher_id"), "POST") == true) {
+    if (count(filter_input(INPUT_POST, "students", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY)) > 0) {
+        foreach (filter_input(INPUT_POST, "students", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY) as $student_id) {
             $admin = new Admin();
-            $admin->assign_teacher($_POST["teacher_id"], $student_id);
+            $admin->assign_teacher(filter_input(INPUT_POST, "teacher_id", FILTER_DEFAULT), $student_id);
             unset($admin);
         }
-        $struct->successBox("Assign Students", "Succesfully assigned the following student(s) to {$_GET["teacher_name"]}!<br>");
+        Structure::successBox("Assign Students", "Succesfully assigned the following student(s) to {$_GET["teacher_name"]}!<br>");
     }
-} elseif ($struct->is_int_present($_GET["teacher_id"]) == true) {
+} elseif (Structure::is_int_present(filter_input(INPUT_GET, "teacher_id", FILTER_DEFAULT)) == true) {
     $teacher = new Admin();
-    if ($teacher->teacher_exists($_GET["teacher_id"]) === true) {
+    if ($teacher->teacher_exists(filter_input(INPUT_GET, "teacher_id", FILTER_DEFAULT)) === true) {
         $students = $teacher->student_with_no_teacher();
         if (count($students) > 0) {
-            echo "<main role=\"main\" class=\"container mt-3  mx-auto\">";
-            echo $struct->topHeading("Assign Teacher");
-            echo "
-      <hr>
-      <form method=\"post\">
-        <input type=\"hidden\" name=\"teacher_id\" value=\"{$_GET['teacher_id']}\">
+            echo('<main role="main" class="container mt-3  mx-auto">');
+            Structure::topHeading("Assign Teacher");
+            echo('<hr>
+            <form method="post">
+              <input type="hidden" name="teacher_id" value="'.filter_input(INPUT_GET, "teacher_id", FILTER_DEFAULT).'">
 
-        <div class=\"row\">
-          <div class=\"col\">
-              <fieldset>
-              <table class=\"table table-striped table-bordered table-hover\">
-              <caption class=\"text-info\">Assign student to <i>{$_GET['teacher_name']}</i></caption>
-                <thead class=\"bg-dark text-white\">
-                  <tr>
-                    <th scope=\"col\" style=\" width: 10%; \">#</th>
-                    <th scope=\"col\">Student Name</th>
-                  </tr>
-                </thead>
-                <tbody>";
+              <div class="row">
+                <div class="col">
+                    <fieldset>
+                    <table class="table table-striped table-bordered table-hover">
+                    <caption class="text-info">Assign student to <i>'.filter_input(INPUT_GET, "teacher_name", FILTER_DEFAULT).'</i></caption>
+                      <thead class="bg-dark text-white">
+                        <tr>
+                          <th scope="col" style=" width: 10%; ">#</th>
+                          <th scope="col">Student Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>');
             foreach ($students as $student) {
-                echo "
-                  <tr>
-                    <td>
-                      <div class=\"form-check\">
-                        <input class=\"form-check-input\" type=\"checkbox\" name=\"students[]\" value=\"{$student['student_id']}\">
-                      </div>
-                    </td>
-                    <td>{$student['student_name']}</a></td>
-                  </tr>";
+                echo('<tr>
+                  <td>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" name="students[]" value="'.$student['student_id'].'">
+                    </div>
+                  </td>
+                  <td>'.$student['student_name'].'</a></td>
+                </tr>');
             }
-            echo "    </tbody>
+            echo('</tbody>
                   </table>
                 </fieldset>
               </div>
-          </div>
+            </div>
 
-          <div class=\"row\">
-            <div class=\"col-sm-12\">
-                <button type=\"submit\" class=\"btn btn-success btn-small\">Submit</button>
-                <a class=\"btn btn-primary btn-small\" href=\"".$struct->nakedURL("view_teachers.php")."\" role=\"button\">Go back!</a>
+            <div class="row">
+            <div class="col-sm-12">
+                <button type="submit" class="btn btn-success btn-small">Submit</button>
+                <a class="btn btn-primary btn-small" href="'.Structure::nakedURL("view_teachers.php").'" role="button">Go back!</a>
              </div>
-          </div>
-      </form>
-      </main>";
+            </div>
+            </form>
+            </main>');
         } else {
-            $struct->errorBox("Assign Students", "No students available!");
+            Structure::errorBox("Assign Students", "No students available!");
         }
     } else {
-        $struct->errorBox("Assign Students", "Not a valid teacher!");
+        Structure::errorBox("Assign Students", "Not a valid teacher!");
     }
 
     $teacher->close_DB();
+    unset($teacher);
 } else {
-    $struct->errorBox("Assign Students", "No teacher selected!");
+    Structure::errorBox("Assign Students", "No teacher selected!");
 }
 
-$struct->footer();
+Structure::footer();
